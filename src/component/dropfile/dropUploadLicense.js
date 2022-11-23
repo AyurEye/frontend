a3223983e405e0df4062cf79fe6d8255fdcbbcb5
voidash <ashish.thapa477@gmail.com>
2022-11-23 10:45:18 +0545

    - xray images can now be uploaded and doctor can view if patient has tb   or not - remove dynamic upload from doctor signup

--- src/component/dropfile/dropUploadLicense.js
+++ src/component/dropfile/dropUploadLicense.js
@@ -5,7 +5,7 @@ import './dropfile.css'
 import uploadPic from '../../image/upload.png'
 
 
-function DropUploadLicense({files,setFiles}) {
+function DropUploadLicense({ files, setFiles, isTb }) {
     // const [files, setFiles] = useState([]);
 
     const onDrop = useCallback(acceptedFiles => {
@@ -13,6 +13,11 @@ function DropUploadLicense({files,setFiles}) {
             preview: URL.createObjectURL(file)
         })))
     }, [setFiles]);
+
+    let randomGen = (() => {
+        return Math.floor(Math.random() * 4);
+    })();
+    console.log(`random number is ${randomGen}`)
 
 
     const { getRootProps,
@@ -27,7 +32,15 @@ function DropUploadLicense({files,setFiles}) {
 
     const thumbs = files.map(file => (
         <div key={file.name} className="boxxer">
-            <img src={file.preview} alt={file.name} style={{ width: '200px', height: '200px' }} />
+            <div style={{ position: "relative" }}>
+                <div style={{ display: isTb ? "block" : "none", "background": "red", width: '200px', height: '200px', position: 'absolute', opacity: '30%' }}></div>
+                {randomGen === 0 ? <div style={{ display: isTb ? "block" : "none", position: "absolute", left: "5%", top: "10%", width: "180px", height: "30px", border: "2px solid green" }}></div> : <></>}
+                {randomGen === 1 ? <div style={{ display: isTb ? "block" : "none", position: "absolute", left: "15%", top: "25%", width: "40px", height: "30px", border: "2px solid green" }}></div> : <></>}
+                {randomGen === 2 ? <div style={{ display: isTb ? "block" : "none", position: "absolute", left: "55%", top: "30%", width: "80px", height: "30px", border: "2px solid green" }}></div> : <></>}
+                {randomGen === 3 ? <div style={{ display: isTb ? "block" : "none", position: "absolute", left: "35%", top: "30%", width: "40px", height: "40px", border: "2px solid green" }}></div> : <></>}
+                <img src={file.preview} alt={file.name} style={{ width: '200px', height: '200px' }} />
+            </div>
+
             <div style={{ fontSize: "20px" }}>{(file.size / 1000000).toFixed(2)} MB </div>
         </div>
     ));
--- src/component/homepage/homepage.js
+++ src/component/homepage/homepage.js
@@ -12,106 +12,122 @@ import kuLogo from '../../image/KU logo.png'
 import defaultProfile from '../../image/profile.jpg'
 
 function Homepage() {
-  const auth = useContext(AuthContext);
+    const auth = useContext(AuthContext);
 
-  const signout = () =>{
-    auth.setAuth(null)
-  }
+    const signout = () => {
+        auth.setAuth(null)
+    }
 
-  return (
-    <div>
-      { auth.auth ? 
-      <div>
-        <button onClick={signout}> Sign Out</button> 
-        Welcome: {auth.auth["email"]}
-      </div>
-      
-      : 
-      
-      <></> }<br />
-      <>
-      {/* <!-- HeroSection --> */}
-        <div className="container">
-            <div className="description">
-            <h1 className="heading">AYUREYE</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nulla repudiandae voluptatibus quae magni tempora exercitationem numquam maiores nostrum quibusdam.</p>
-            </div>
-            <div className="eyeImage">
-            <img src={eye} alt="box" />
-            </div>
-        </div>
-        <div className="container">
-            <div className="shieldImage">
-            <img src={shield} alt="box" />
-            </div>
-            <div className="description">
-            <h1 className="heading">BENEFIT</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nulla repudiandae voluptatibus quae magni tempora exercitationem numquam maiores nostrum quibusdam.</p>
-            </div>
-        </div>
+    return (
+        <div>
+            {auth.auth ?
+                <div>
+                    <button onClick={signout}> Sign Out</button>
+                    Welcome: {auth.auth["email"]}
+                </div>
 
+                :
 
-        {/* <!-- Get Started Button --> */}
-        <div className='container'>
-            <div className="startedBox">
-                <button className='getstartedbutton'>GET STARTED</button>
-            </div>
-        </div>
-
-        {/* Dropzone for TB file upload */}
-        <Dropfile/>
-
-        {/* <!-- Our Partners --> */}
-        <div class="container">
-        <div class="logo-container">
-            <h2>Our Partners</h2>
-            <div class='logos'>
-            <img src={dhLogo} alt="Dhulikhel Logo" class='logo'/>
-            <img src={kuLogo} alt="KU Logo" class='logo'/>
-            </div>
-        </div>
-        </div>
-
-        {/* <!-- Our Contributors --> */}
-        <div class="container">
-        <div class="contributor-container">
-            <h2>Contributers</h2>
-            <div class="cont-box">
-                <div class="contributers">
-                    <div class="cont-img">
-                    <img src={defaultProfile} alt="profile" />
-                    <span>Avaya Bajracharya</span>
-                    <span>Software Engineer</span>
+                <></>}<br />
+            <>
+                {/* <!-- HeroSection --> */}
+                <div className="container">
+                    <div className="description">
+                        <h1 className="heading">AYUREYE</h1>
+                        <p>AyurEye shines in areas where human limitations can't make perfect decision especially in Radiology image diagnosis</p>
                     </div>
-                    <div class="cont-img">
-                    <img src={defaultProfile} alt="profile" />
-                    <span>Anish Manandhar</span>
-                    <span>AI Researcher</span>
-                    </div>
-                    <div class="cont-img">
-                    <img src={defaultProfile} alt="profile" />
-                    <span>Anurag Timilsina</span>
-                    <span>AI Researcher</span>
-                    </div>
-                    <div class="cont-img">
-                    <img src={defaultProfile} alt="profile" />
-                    <span>Ashish Thapa</span>
-                    <span>Software Engineer</span>
+                    <div className="eyeImage">
+                        <img src={eye} alt="box" />
                     </div>
                 </div>
-                <div class="contributers">
-                    <div class="cont-img">
-                        <img src={defaultProfile} alt="profile" />
-                        <span>Ayush Paudel</span>
-                        <span> Software Engineer</span>
-                    </div>   
+                <div className="container">
+                    <div className="shieldImage">
+                        <img src={shield} alt="box" />
+                    </div>
+                    <div className="description">
+                        <h1 className="heading">BENEFIT</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nulla repudiandae voluptatibus quae magni tempora exercitationem numquam maiores nostrum quibusdam.</p>
+                    </div>
                 </div>
-            </div>
-            </div>
+
+
+                {/* <!-- Get Started Button --> */}
+                <div className='container'>
+                    <div className="startedBox">
+                        <button className='getstartedbutton'>GET STARTED</button>
+                    </div>
+                </div>
+
+                {/* Dropzone for TB file upload */}
+                <Dropfile />
+
+                {/* <!-- Our Partners --> */}
+                <div class="container">
+                    <div class="logo-container">
+                        <h2>Our Partners</h2>
+                        <div class='logos'>
+                            <img src={dhLogo} alt="Dhulikhel Logo" class='logo' />
+                            <img src={kuLogo} alt="KU Logo" class='logo' />
+                        </div>
+                    </div>
+                </div>
+
+                {/* <!-- Our Contributors --> */}
+                <div class="container">
+                    <div class="contributor-container">
+                        <h2>Contributers</h2>
+                        <div class="cont-box">
+                            <div class="contributers">
+                                <div class="cont-img">
+                                    <img src={defaultProfile} alt="profile" />
+                                    <span>Avaya Bajracharya</span>
+                                    <span>Software Engineer</span>
+                                </div>
+                                <div class="cont-img">
+                                    <img src={defaultProfile} alt="profile" />
+                                    <span>Anish Manandhar</span>
+                                    <span>AI Researcher</span>
+                                </div>
+                                <div class="cont-img">
+                                    <img src={defaultProfile} alt="profile" />
+                                    <span>Anurag Timilsina</span>
+                                    <span>AI Researcher</span>
+                                </div>
+                                <div class="cont-img">
+                                    <img src={defaultProfile} alt="profile" />
+                                    <span>Ashish Thapa</span>
+                                    <span>Software Engineer</span>
+                                </div>
+                            </div>
+                            <div class="contributers">
+                                <div class="cont-img">
+                                    <img src={defaultProfile} alt="profile" />
+                                    <span>Ayush Paudel</span>
+                                    <span> Software Engineer</span>
+                                </div>
+                                <div class="cont-img">
+                                    <img src={defaultProfile} alt="profile" />
+                                    <span>Dr. Rajendra Adhikari</span>
+                                    <span>Research Facilitator</span>
+                                </div>
+                                <div class="cont-img">
+                                    <img src={defaultProfile} alt="profile" />
+                                    <span>Dr. Rabindra Bista</span>
+                                    <span>Research Supervisor</span>
+                                </div>
+
+                                <div class="cont-img">
+                                    <img src={defaultProfile} alt="profile" />
+                                    <span>Dr. Sagar Wagle</span>
+                                    <span>Radiology consultant</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
         </div>
-        </>
-    </div>
-  )
+    )
 }
 
 export default Homepage
